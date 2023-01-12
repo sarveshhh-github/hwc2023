3e8fb1863636aa1ef36997c1a5a73a901ef96412
Sarvesh Savaliya <sarveshsavaliya1541@gmail.com>
2023-01-12 10:43:54 +0530

    added Routes with createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-
-    <h1>hello</h1>)
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/pool-a",
+    element: <div>pool A</div>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
