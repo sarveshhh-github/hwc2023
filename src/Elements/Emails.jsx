6fe237776503c00bca2a20954bd40cbb903d2869
Sarvesh Savaliya <sarveshsavaliya1541@gmail.com>
2023-01-18 18:51:11 +0530

    emails of poco m2

---
+++ src/Elements/Emails.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Emails = () => {
+  return (
+    <div>
+      <h1>sarveshsavaliya1541@gmail.com</h1>
+      <h1>iglost999@gmail.com</h1>
+      <h1>angelandlost2002@gmail.com</h1>
+      <h1>dadaa8718@gmail.com</h1>
+      <h1>sports.safar@gmail.com</h1>
+    </div>
+  );
+}
+
+export default Emails;
